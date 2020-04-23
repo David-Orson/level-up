a1ff649ecd007baf306b4b176570df8301701a2a
David Orson <onlyorson@outlook.com>
2020-04-23 17:12:52 +0100

    Nav Links

--- app.js
+++ app.js
@@ -1,2 +1,77 @@
 "use strict";
-console.log("hi");
+// State
+var state = {
+    signedIn: "false",
+    main: "home",
+};
+// Nav
+var root = document.querySelector("#root");
+var nav = document.createElement("nav");
+var navContainer = document.createElement("div");
+var logoContainer = document.createElement("div");
+var title1 = document.createElement("h2");
+var title2 = document.createElement("span");
+title1.textContent = "LevelUp ";
+title2.textContent = "+";
+title1.setAttribute("class", "logo");
+title2.setAttribute("class", "font-primary");
+navContainer.setAttribute("class", "nav-container");
+navContainer.appendChild(title1);
+title1.appendChild(title2);
+root === null || root === void 0 ? void 0 : root.appendChild(nav);
+nav === null || nav === void 0 ? void 0 : nav.appendChild(navContainer);
+title1.addEventListener("click", function () {
+    state.main = "home";
+    /* mainRender(state); */
+});
+// Signing Links
+var linksRender = function (state) {
+    console.log(state);
+    var signIn = document.createElement("button");
+    var signedInLinks = document.createElement("div");
+    var signOut = document.createElement("button");
+    var user = document.createElement("img");
+    var userName = document.createElement("span");
+    if (state.signedIn === "false") {
+        var out = document.querySelector("#signedIn");
+        if (out) {
+            navContainer.removeChild(out);
+        }
+        signIn.textContent = "SIGNIN";
+        signIn.setAttribute("id", "signin");
+        signIn.setAttribute("class", "btn");
+        navContainer === null || navContainer === void 0 ? void 0 : navContainer.appendChild(signIn);
+        signIn.addEventListener("click", function () {
+            state.main = "signin";
+            /* mainRender(state); */
+            // temp code until main setup
+            state.signedIn = "true";
+            linksRender(state);
+        });
+    }
+    else {
+        var myIn = document.querySelector("#signin");
+        if (myIn) {
+            navContainer.removeChild(myIn);
+        }
+        signedInLinks.setAttribute("id", "signedIn");
+        signedInLinks.setAttribute("class", "signedIn");
+        signOut.textContent = "LOGOUT";
+        signOut.setAttribute("id", "logout");
+        signOut.setAttribute("class", "btn");
+        user.setAttribute("id", "user");
+        user.setAttribute("src", "./images/logo11.png");
+        user.setAttribute("class", "user");
+        user.setAttribute("height", "50px");
+        user.setAttribute("width", "50px");
+        navContainer === null || navContainer === void 0 ? void 0 : navContainer.appendChild(signedInLinks);
+        signedInLinks === null || signedInLinks === void 0 ? void 0 : signedInLinks.appendChild(user);
+        signedInLinks === null || signedInLinks === void 0 ? void 0 : signedInLinks.appendChild(signOut);
+        signOut.addEventListener("click", function () {
+            state.signedIn = "false";
+            linksRender(state);
+            /* mainRender(state); */
+        });
+    }
+};
+linksRender(state);
--- app.ts
+++ app.ts
@@ -1,1 +1,95 @@
-console.log("hi");
+// State
+
+const state = {
+  signedIn: "false",
+  main: "home",
+};
+
+// Nav
+
+const root: HTMLElement | null = document.querySelector("#root");
+const nav = document.createElement("nav");
+const navContainer = document.createElement("div");
+const logoContainer = document.createElement("div");
+const title1: HTMLElement = document.createElement("h2");
+const title2 = document.createElement("span");
+
+title1.textContent = "LevelUp ";
+title2.textContent = "+";
+
+title1.setAttribute("class", "logo");
+title2.setAttribute("class", "font-primary");
+navContainer.setAttribute("class", "nav-container");
+
+navContainer.appendChild(title1);
+title1.appendChild(title2);
+
+root?.appendChild(nav);
+nav?.appendChild(navContainer);
+
+title1.addEventListener("click", () => {
+  state.main = "home";
+  /* mainRender(state); */
+});
+
+// Signing Links
+
+const linksRender = (state: any) => {
+  console.log(state);
+  const signIn = document.createElement("button");
+
+  const signedInLinks = document.createElement("div");
+  const signOut = document.createElement("button");
+  const user = document.createElement("img");
+  const userName = document.createElement("span");
+  if (state.signedIn === "false") {
+    let out = document.querySelector("#signedIn");
+    if (out) {
+      navContainer.removeChild(out);
+    }
+    signIn.textContent = "SIGNIN";
+    signIn.setAttribute("id", "signin");
+    signIn.setAttribute("class", "btn");
+
+    navContainer?.appendChild(signIn);
+
+    signIn.addEventListener("click", () => {
+      state.main = "signin";
+      /* mainRender(state); */
+      // temp code until main setup
+
+      state.signedIn = "true";
+      linksRender(state);
+    });
+  } else {
+    let myIn = document.querySelector("#signin");
+
+    if (myIn) {
+      navContainer.removeChild(myIn);
+    }
+    signedInLinks.setAttribute("id", "signedIn");
+    signedInLinks.setAttribute("class", "signedIn");
+
+    signOut.textContent = "LOGOUT";
+    signOut.setAttribute("id", "logout");
+    signOut.setAttribute("class", "btn");
+
+    user.setAttribute("id", "user");
+    user.setAttribute("src", "./images/logo11.png");
+    user.setAttribute("class", "user");
+    user.setAttribute("height", "50px");
+    user.setAttribute("width", "50px");
+
+    navContainer?.appendChild(signedInLinks);
+    signedInLinks?.appendChild(user);
+    signedInLinks?.appendChild(signOut);
+
+    signOut.addEventListener("click", () => {
+      state.signedIn = "false";
+      linksRender(state);
+      /* mainRender(state); */
+    });
+  }
+};
+
+linksRender(state);
